0d666f2a9bbe4c994cfd2cf9407d9b5c413d2892
Dolf <eric.jonsson.hederstedt@gmail.com>
2024-06-23 20:58:36 +0200

    asd

---
+++ mod.cpp
@@ -0,0 +1,17 @@
+name = "Viorla Unit Mod 2 Electric Boogaloo";
+//picture = "picture.paa";"
+//logoSmall = "logoSmall.paa";
+//logo = "logo.paa";
+//logoOver = "logoOver.paa";
+action = "";
+tooltipOwned = "Vior'la Fire Warriors";
+dlcColor[] = 
+{
+	0, // Red
+	0.15, // Green
+	1, // Blue
+	1 // Alpha
+};
+overview = "Mod for the Viorla unit";
+hideName = 0;
+hidePicture = 0;
